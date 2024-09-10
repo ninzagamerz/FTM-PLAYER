970e7dcbea4907acd1d8de72534aba574a652aef
ninzagamerz <106005123+ninzagamerz@users.noreply.github.com>
2024-09-10 21:55:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -11,45 +11,45 @@ if os.path.exists("local.env"):
 load_dotenv()
 admins = {}
 
-SESSION_NAME = getenv("SESSION_NAME")
+SESSION_NAME = getenv("SESSION_NAME", "FTM")
 
-if str(getenv("STRING_SESSION2")).strip() == "":
+if str(getenv("STRING_SESSION2")).strip() == "FTM1":
     SESSION2 = str(None)
 else:
     SESSION2 = str(getenv("STRING_SESSION2"))
 
-if str(getenv("STRING_SESSION3")).strip() == "":
+if str(getenv("STRING_SESSION3")).strip() == "FTM3":
     SESSION3 = str(None)
 else:
     SESSION3 = str(getenv("STRING_SESSION3"))
 
-if str(getenv("STRING_SESSION4")).strip() == "":
+if str(getenv("STRING_SESSION4")).strip() == "FTM4":
     SESSION4 = str(None)
 else:
     SESSION4 = str(getenv("STRING_SESSION4"))
 
-if str(getenv("STRING_SESSION5")).strip() == "":
+if str(getenv("STRING_SESSION5")).strip() == "FTM5":
     SESSION5 = str(None)
 else:
     SESSION5 = str(getenv("STRING_SESSION5"))
 
-BOT_TOKEN = getenv("BOT_TOKEN", "")
-BOT_NAME = getenv("BOT_NAME", "Umk")
+BOT_TOKEN = getenv("BOT_TOKEN", "7449988437:AAFFxdpVdedmUt0pcX0YDBrbC2jbnYwTq9E")
+BOT_NAME = getenv("BOT_NAME", "FTM PLAYER")
 
-API_ID = int(getenv("API_ID", "8186557"))
-API_HASH = getenv("API_HASH", "efd77b34c69c164ce158037ff5a0d117")
-MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://Cloner:Cloner@cluster0.cgc6t.mongodb.net/?retryWrites=true&w=majority")
-OWNER_NAME = getenv("OWNER_NAME", "Zaid")
-OWNER_USERNAME = getenv("OWNER_USERNAME", "Timesisnotwaiting")
-ALIVE_NAME = getenv("ALIVE_NAME", "Zaid")
-BOT_USERNAME = getenv("BOT_USERNAME", "Zaid2_Robot")
-OWNER_ID = getenv("OWNER_ID", "1669178360")
-ASSISTANT_NAME = getenv("ASSISTANT_NAME", "Zaid2_Assistant")
-GROUP_SUPPORT = getenv("GROUP_SUPPORT", "TheSupportChat")
-UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "TheUpdatesChannel")
+API_ID = int(getenv("API_ID", "28776072"))
+API_HASH = getenv("API_HASH", "b3a786dce1f4e7d56674b7cadfde3c9d")
+MONGO_DB_URL = getenv("MONGO_DB_URL", "mongodb+srv://ftm:ftm@cluster0.rhh9r.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+OWNER_NAME = getenv("OWNER_NAME", "╚» 𝔽𝕋𝕄 𝔻𝔼𝕍𝔼𝕃𝕆ℙ𝔼ℝ«╝")
+OWNER_USERNAME = getenv("OWNER_USERNAME", "ftmdeveloper")
+ALIVE_NAME = getenv("ALIVE_NAME", "FTM")
+BOT_USERNAME = getenv("BOT_USERNAME", "ftmplayerbot")
+OWNER_ID = getenv("OWNER_ID", "7042535787")
+ASSISTANT_NAME = getenv("ASSISTANT_NAME", "FTM_ASSISTANT")
+GROUP_SUPPORT = getenv("GROUP_SUPPORT", "FTM MOVIES WORLD")
+UPDATES_CHANNEL = getenv("UPDATES_CHANNEL", "ftmmovieworldofficial")
 HEROKU_APP_NAME = getenv("HEROKU_APP_NAME")
 HEROKU_API_KEY = getenv("UPDATES_CHANNEL", "HEROKU_API_KEY")
-SUDO_USERS = list(map(int, getenv("SUDO_USERS", "1669178360").split()))
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "7042535787").split()))
 COMMAND_PREFIXES = list(getenv("COMMAND_PREFIXES", "/ ! .").split())
 ALIVE_IMG = getenv("ALIVE_IMG", "https://telegra.ph/file/fc9d87ffd1c6f828eb7fc.png")
 START_PIC = getenv("START_PIC", "https://telegra.ph/file/a414e2cdfeaa7d4414b89.png")
